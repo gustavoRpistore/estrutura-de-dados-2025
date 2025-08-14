ae2dbd2b1d0454a2356a37f836c2dc2fdba390c5
Gustavo Ribeiro Pistore <451431@unoesc.lan>
2025-08-13 21:44:17 -0300

    jaggedArrays

---
+++ jaggedArrays/Program.cs
@@ -0,0 +1,30 @@
+﻿// jaggedArrays 
+// sao arrays de arrays 
+// ou tambem arrays irregulares 
+int [][] numbers =
+ new int[4][];
+ numbers[0] =
+  new int [] {1,2,3 };
+numbers[1] = 
+ new int [] {5,6,7,8,9};
+ numbers[3] = 
+   new int []{6,5,4};
+   int[][] numbers 
+
+   //outra forma de inicializar
+   int [][] numbers2 =
+   {
+        new int []{1},
+        new int []{2},
+        new int []{3},
+        new int []{4},
+   };
+   for(int i = 0; i < numbers.length; i++)
+   {
+    console.write ("[")
+    fpr (int j = 0; j < numbers[i].length; j+=)
+    {
+        console.write( $"{numers[i][j]} ");
+   }
+  console.write("[");
+   }
