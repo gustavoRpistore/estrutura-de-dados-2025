4c8901afd8d6e17b303e06b5d4a0f5d3df8d6b1e
gustavoRpistore <gstv8845@gmail.com>
2025-08-20 21:56:25 -0300

    Add files via upload

---
+++ recursive2/codigo3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace RecursionDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            fun2(x);
+            Console.ReadKey();
+        }
+
+        static void fun2(int n)
+        {
+            if (n > 0)
+            {
+                fun2(n - 1);
+                Console.Write($"{n} ");
+            }
+        }
+    }
+}
