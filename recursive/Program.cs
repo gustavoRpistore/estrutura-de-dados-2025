3c267c88984516450f6b4c3afde10137513f9102
gustavoRpistore <gstv8845@gmail.com>
2025-08-20 21:13:42 -0300

    Add files via upload

---
+++ recursive/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("enter the length of the fibonacci series ");
+int length = convert.ToInt32(console.Readline());
+for (int i = 0; < length; i++)
+{
+    console.write("{0}", fibonacciSeries);
+}
+console.Readkay();
+
+static int fibonacciSeries(int n)
+{
+if ( n == 0 ) return 0; // to the return the first fibonacci number 
+if ( n ==1 ) return 1; // to the return the second fibonacci number
+return  fibonacciSeries(n - 1 ) + fibonacciSeries(n - 2);
+}10
