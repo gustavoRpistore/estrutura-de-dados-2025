90b8fc62bf659bd1040818436f33327d4f4f97f7
gustavoRpistore <gstv8845@gmail.com>
2025-09-03 21:41:55 -0300

    Add files via upload

---
+++ palindromo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+Console.WriteLine("digite uma palavra");
+string palavra = Console.ReadLine();
+
+Stack<char> chars = new Stack<char>();
+foreach (var c in  palavra )
+
+    chars.Push(c);
+
+string palavra_invertida = string.Empty;
+while (chars.Count > 0)
+{
+    char c = chars.Pop();
+    palavra_invertida += c;
+}
+
+if (palavra == palavra_invertida)
+    Console.WriteLine("a palavra e um polindromo");
+else
+    Console.WriteLine("a palavra nao e um poindromo");
+
+
