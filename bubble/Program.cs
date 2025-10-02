5c41934af9a62c6269ba7c6c1492d9486f47962f
gustavoRpistore <gstv8845@gmail.com>
2025-10-01 21:54:46 -0300

    Add files via upload

---
+++ bubble/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using bubble;
+
+Console.WriteLine("Ordenção com Bobble");
+int[] arrNumber = new int[] { 99, 50, -24, 0, 1 };
+string[] arrString = new string[] { "gustavo", "daniel", "bruno", "igor" };
+
+foreach (var number in arrNumber)
+{
+    Console.Write($"[{number}]");
+}
+
+var arOrdered = BubbleSortOrder.Sort<int>(arrNumber);
+
+var arrStringordered = BubbleSortOrder.Sort<string>(arrString);
+Console.WriteLine("vetor ordenado");
+
+foreach (var number in arOrdered)
+{
+    Console.Write($"[{number}]");
+}
+
+foreach (var str in arrStringordered)
+{
+    Console.Write($"[{str}]");
+}
